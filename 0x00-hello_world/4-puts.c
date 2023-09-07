86343d9db8439dc3ecdb9e26f27f5dfa64d98e9a
hemasallam03@gmail.com <hemasallam03@gmail.com>
2023-09-07 15:27:43 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main(void)
+/*
+ *Description: Uses the function "puts" to write to standrd out put
+ * */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
