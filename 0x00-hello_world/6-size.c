e0e895647ac57cb0d76f78e60fdb5a976dd8d437
0xHemaSallam <hemasallam03@gmail.com>
2023-12-18 22:03:27 +0200

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ * main - is the entry point of the code
+ * 
+ * Decreption: Use "sizeof" function to get size of type
+ * Return: Always(0)
+*/
+
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
+	char a;
+	int b;
+	long int c;
+	long long d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(d));
-	printf("Size of an int: %lu byte(s)\n", sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)",sizeof(a));
+	printf("Size of a int: %lu byte(s)",sizeof(b));
+	printf("Size of a long int: %lu byte(s)",sizeof(c));
+	printf("Size of a long long: %lu byte(s)",sizeof(d));
+	printf("Size of a float: %lu byte(s)",sizeof(f));
 	return (0);
 }
